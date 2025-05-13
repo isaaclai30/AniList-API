2ae1b2a5242a52bea59a93d8518e49faad0e6c76
Isaac Lai <ichenlai3.0@gmail.com>
2025-05-13 12:27:26 -0400

    1st revision
    .vscode/ added to .gitignore and comments and code changes

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import requests
 
 def lambda_handler():
+  #main call in order formatted for ans
   media = call_anilist_api()
   check_upcoming_episodes(media)
   
   
 def call_anilist_api():
+  userId = 7322485
+  #environment variable for changing which userId is pulled
+  
   has_next_line = True
   page = 0
+  #variable for potential loops
+  
   media = []
+  #output of the function
+  
   while has_next_line:
     page+=1
     query = '''
@@ -35,37 +43,47 @@ def call_anilist_api():
         }
       }
     }
-    '''
+    ''' 
+    #Graphql schema call for anilist api
+    
     variables = {
-      "userId": 7322485,
+      "userId": userId,
       "page": page,
       "perPage": 50,
       "status": "CURRENT",
       "sort": "MEDIA_TITLE_ROMAJI",
       "type": "ANIME"
     }
+    #Varibles inputed into the request page and userId being variables
+    
     url = 'https://graphql.anilist.co'
 
     response = requests.post(url, json={'query': query, 'variables': variables})
     data = response.json()
     media.extend(data["data"]["Page"]["mediaList"])
+    #api call and saving series output
     
-    has_next_line = data["data"]["Page"]["pageInfo"]["hasNextPage"]
-  return media    
+    has_next_line = data["data"]["Page"]["pageInfo"]["hasNextPage"] 
+    #check if more data to check on more pages
+  return media
 
 def check_upcoming_episodes(media):
   for series in media:
-    if series["media"]["nextAiringEpisode"] != None and series["media"]["nextAiringEpisode"]["timeUntilAiring"] < 300:
-      send_webhook(series[media])
+    if series["media"]["nextAiringEpisode"] != None and series["media"]["nextAiringEpisode"]["timeUntilAiring"] < 3000000:
+      send_webhook(series["media"])
+      #check all curently watching series to check if it is ongoing and if it is going to release within 5 minutes, then calls for a webhook 
     
 def send_webhook(series):
   discord_webhook = "https://discord.com/api/webhooks/1371573170825593037/ZQXyMtptqMCmNzjwev4_uZ40nolBE8-6sRTiNAZ1-NBVOB5OTc2W4c9-SRcNpmBYsEnQ"
-  time_until_airing = series["nextAiringEposide"]["timeUntilAiring"]
+  #enviromental variable for changing the webhook used
+  
+  time_until_airing = series["nextAiringEpisode"]["timeUntilAiring"]
+  #varible used next line to shorten, pull the next episode coming out and check when it is excepted to release
+  
   body = series["title"]["romaji"] + " will be releasing in " + str(time_until_airing/60) + " mintues and " + str(time_until_airing%60) + " seconds approximately." 
-  requests.post(discord_webhook, json={"content": body})
-  requests.post(discord_webhook, json={"content": series["coverImage"]["large"]})
+  requests.post(discord_webhook, json={"content": body, "image": series["coverImage"]["large"]})
+  #call to webhook to send out series going to release soon
   
   
 
 lambda_handler()
-requests.post("https://discord.com/api/webhooks/1371573170825593037/ZQXyMtptqMCmNzjwev4_uZ40nolBE8-6sRTiNAZ1-NBVOB5OTc2W4c9-SRcNpmBYsEnQ", json={"content": "text"})
