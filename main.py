ad3e56d0aee39661759e02bec86312888a64a5da
Isaac Lai <ichenlai3.0@gmail.com>
2025-05-14 21:10:29 -0400

    workflow push

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import os
 import requests
 
-def lambda_handler():
+def lambda_handler(event, context):
   #main call in order formatted for ans
   media = call_anilist_api()
   check_upcoming_episodes(media)
   
   
 def call_anilist_api():
-  userId = 7322485
+  userId = os.environ.get("USER_ID")
   #environment variable for changing which userId is pulled
   
   has_next_line = True
@@ -58,10 +59,17 @@ def call_anilist_api():
     
     url = 'https://graphql.anilist.co'
 
-    response = requests.post(url, json={'query': query, 'variables': variables})
-    data = response.json()
+    try:
+      response = requests.post(url, json={'query': query, 'variables': variables})
+      response.raise_for_status()
+      data = response.json()
+      #api call
+    except requests.RequestException as e:
+      print(f"Request failed: {e}")
+      return []
+    #error handling
     media.extend(data["data"]["Page"]["mediaList"])
-    #api call and saving series output
+    #saving series output
     
     has_next_line = data["data"]["Page"]["pageInfo"]["hasNextPage"] 
     #check if more data to check on more pages
@@ -69,21 +77,18 @@ def call_anilist_api():
 
 def check_upcoming_episodes(media):
   for series in media:
-    if series["media"]["nextAiringEpisode"] != None and series["media"]["nextAiringEpisode"]["timeUntilAiring"] < 3000000:
+    if series["media"]["nextAiringEpisode"] != None and series["media"]["nextAiringEpisode"]["timeUntilAiring"] < 300:
       send_webhook(series["media"])
       #check all curently watching series to check if it is ongoing and if it is going to release within 5 minutes, then calls for a webhook 
     
 def send_webhook(series):
-  discord_webhook = "https://discord.com/api/webhooks/1371573170825593037/ZQXyMtptqMCmNzjwev4_uZ40nolBE8-6sRTiNAZ1-NBVOB5OTc2W4c9-SRcNpmBYsEnQ"
+  discord_webhook = os.environ.get("WEBHOOK")
   #enviromental variable for changing the webhook used
   
   time_until_airing = series["nextAiringEpisode"]["timeUntilAiring"]
   #varible used next line to shorten, pull the next episode coming out and check when it is excepted to release
   
-  body = series["title"]["romaji"] + " will be releasing in " + str(time_until_airing/60) + " mintues and " + str(time_until_airing%60) + " seconds approximately." 
-  requests.post(discord_webhook, json={"content": body, "image": series["coverImage"]["large"]})
+  body = series["title"]["romaji"] + " will be releasing in " + str(int(time_until_airing/60)) + " mintues and " + str(time_until_airing%60) + " seconds approximately." 
+  requests.post(discord_webhook, json={"content": body})
+  requests.post(discord_webhook, json={"content": series["coverImage"]["large"]})
   #call to webhook to send out series going to release soon
-  
-  
-
-lambda_handler()
